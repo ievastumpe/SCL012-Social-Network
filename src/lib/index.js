3110cbfbc5995f24e43fb26a4edf8aba62f188bd
katsmunoz <katherine.smm@gmail.com>
2020-01-20 16:03:32 -0300

    Tutorial de registro, ingreso y cerrar sesion

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,84 @@
 // aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
-  // aqui tu codigo
-  console.log('Hola mundo!');
+export const registrar = () => {
+  const email = document.getElementById('email').value;
+  console.log(email);
+  const password = document.getElementById('password').value;
+  console.log(password);
+
+// Autentificar utilizando Firebase
+firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  const errorCode = error.code;
+  console.log(errorCode);
+  const errorMessage = error.message;
+  console.log(errorMessage);
+  // ...
+})
+
+};
+
+export const ingreso = () => {
+  const email2 = document.getElementById('email2').value;
+  console.log(email2);
+  const password2 = document.getElementById('password2').value;
+  console.log(password2); 
+
+  firebase.auth().signInWithEmailAndPassword(email2, password2).catch(function(error) {
+    // Handle Errors here.
+    const errorCode = error.code;
+    console.log(errorCode);
+    const errorMessage = error.message;
+    console.log(errorMessage);
+    // ...
+  });
+  
+};
+
+export const cerrar = () => {
+  firebase.auth().signOut()
+  .then(function(){
+      console.log('Saliendo...')
+
+  })
+  .catch(function(error){
+      console.log(error)
+
+  })
+};
+
+export const aparece = () => {
+
+  document.getElementById('logOut').addEventListener('click', cerrar);
+
+  const contenido = document.getElementById('content');
+  contenido.innerHTML = `
+  <p>Bienvenido</p>
+  <button id="logOut">Cerrar Sesión</button>
+  
+  `
+};
+
+export const observador = () => {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log('existe usuario activo')
+      // En caso de que el usuario esté activo
+      aparece()
+      // User is signed in.
+      const displayName = user.displayName;
+      const email = user.email;
+      console.log(user);
+      const emailVerified = user.emailVerified;
+      const photoURL = user.photoURL;
+      const isAnonymous = user.isAnonymous;
+      const uid = user.uid;
+      const providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      console.log('no existe usuario activo')
+      // ...
+    }
+  })
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,35 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { registrar } from './lib/index.js';
+import { ingreso } from './lib/index.js';
+import { aparece } from './lib/index.js';
+import { observador } from './lib/index.js';
+import { cerrar } from './lib/index.js';
 
-myFunction();
+// Registro de usuario
+
+document.getElementById('login').addEventListener('click', registrar);
+
+// Ingreso de usuario
+
+document.getElementById('login2').addEventListener('click', ingreso);
+
+// Implementar observador para verificar si el usuario está activo en la página
+
+observador();
+
+// Cerrar sesión
+
+// document.getElementById('logOut').addEventListener('click', cerrar);
+
+// myFunction();
+
+// Usuario con sesión activa
+
+// firebase.auth().onAuthStateChanged(function(user) {
+//   if (user) {
+//     // User is signed in.
+//   } else {
+//     // No user is signed in.
+//   }
+// });
