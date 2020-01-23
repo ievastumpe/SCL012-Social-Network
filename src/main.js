bd9c1e4dfbf9dfac4bd05370cfdc6b5c21fe34cb
“ievastumpe” <ieva.stumpe@gmail.com>
2020-01-23 10:16:55 -0300

    aproving changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,52 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { registrate } from './lib/index.js';
-import { login } from './lib/index.js';
-import { observe } from './lib/index.js';
+import { registrar } from './lib/index.js';
+import { ingreso } from './lib/index.js';
+import { observador } from './lib/index.js';
+import { cerrar } from './lib/index.js';   
+import colegios from './colegios.js';
+import{selectionComunas} from './app.js';
+console.log(colegios)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // Funcion observador que detecta si hay sesión iniciada
+
+observador();
+
+// function aparece(){
+//     const contenido = document.getElementById("contenido");
+//     contenido.innerHTML = `
+//     <p>Bienvenido!</p>
+//     <button onclick="cerrar()">Cerrar Sesión</button>
+//     `;
+// }
+//Api//
