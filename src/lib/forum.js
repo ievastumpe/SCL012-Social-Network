118937a73747f6c82f4096ab96d4514cb653149f
“ievastumpe” <ieva.stumpe@gmail.com>
2020-02-02 17:15:02 -0300

    cambios en archivo forum - todo como comentarios

--- src/lib/forum.js
+++ src/lib/forum.js
@@ -1,58 +1,75 @@
-const database = firebase.firestore();
-const contentForum = document.getElementById('content'); 
 
-const createPost = () => {
-    window.location.hash = '#forum';
-    const input = document.createElement('input');
-    input.type = 'text';
-    contentForum.appendChild(input);
-    const btnSave = document.createElement('button');
-    btnSave.innerHTML = 'Save Post';
-    btnSave.addEventListener('click', () => {
-      const textToSave = input.value;
-      console.log(textToSave);
-      savePost(textToSave);
-      sendPost(textToSave);
-    });
-    contentForum.appendChild(btnSave);
-  };
+// export const templateForum = (cb) => {
+//   console.log('holaaa Forum');
+//   window.location.hash = '#forum';
+//   const containerForum = document.createElement('div');
+//   containerForum.setAttribute('class', 'content');
+//   const contentForum = `
+//     <header>
+//     <div class="logo">
+//     <img class="apiLogoStyle" src="img/fontApoderapi.png" alt="apoderapiLogo">
+//     </div>
+//     </header>
   
-  const savePost = (textPost) => {
-    const texToSave = textPost;
-    console.log(`saving ${texToSave} to Firestore database`);
-    database.collection('post').add({
-      post: texToSave,
-    })
-      .then((docRef) => {
-        console.log('Status Saved!');
-        console.log('Document written with ID: ', docRef.id);
-      })
-      .catch((error) => {
-        console.error('Error adding document: ', error);
-      });
-  };
+//     <div class="register-box">
+//     <main>
+//     <h1 class="titleForm">Forum</h1>
+//     <form>
+//     <p>Escribe tu post aqui</p>
+//     <input class="inputForm" type="textfield" id="postedText" placeholder="Escribe tu texto" class="input" required>
+//     <button class="btnPostStyle" id="addPost" class="btn">Agregar Post</button><br>
+//     <p id="outputField">Your posted message:</p>
+//     </form>
+//     </main>
+//     </div>`;
+ 
+//   document.getElementById("content").innerHTML = contentForum;
+
+//   setTimeout(() => {
+//     const firestore = firebase.firestore();
+//     const btnAddPost = containerForum.querySelector('#addPost');
+//     const outputField = containerForum.querySelector('#outputField');
+//     const inputTextField = containerForum.querySelector('#postedText');
+//     const docRef = firestore.doc('samples/postdata');
   
+//   //------------------Saving written text in the post------------------
+//   btnAddPost.addEventListener('click', () => {
+//     console.log('agregar post');
+//       const textToSave = inputTextField.value;
+//     console.log('I am going to save' + texToSave + 'to Firestore');
+//       outputField (textToSave);
+//       docRef.set({
+//       writtenPost: textToSave
+//       }).then(function() {
+//         console.log('status saved');
+//       }).catch(function (error){
+//         console.log('get an error: ', error);
+//     });
+//   });
   
-  const contentMessage = document.getElementById('content');
-  
-  const sendPost = (textPost) => {
-    const texToSave = textPost;
-    console.log(`saving ${texToSave} to Firestore database`);
-    database.collection('post')
-      .onSnapshot((querySnapshot) => {
-        contentMessage.innerHTML = '';
-        querySnapshot.forEach((doc) => {
-          console.log(doc.id, ' => ', doc.data());
-          contentMessage.innerHTML += `
-              <div>
-              <div class="message"> ${doc.data().post}</div>
-              <td> <button class="btnClear"></button>Editar</button>
-              <td> <button class="btnClear"></button>Eliminar</button>
-              </div>
-              `;
-        });
-      })
-      .catch((error) => {
-        console.error('Error adding document: ', error);
-      });
-  };
+//   cb(containerForum)
+//   }, 0);
+// };
+
+// /*
+//   const sendPost = (textPost) => {
+//     const texToSave = textPost;
+//     console.log(`I am going to save ${texToSave} to Firestore`);
+//     database.collection('post').get().then((querySnapshot) => {
+//         pulishedPost.innerHTML = '';
+//         querySnapshot.forEach((doc) => {
+//           console.log(`${doc.id} => ${doc.data()}`);
+//         pulishedPost.innerHTML += `
+//             <div>
+//             <div class="message"> ${doc.data().POST}</div>
+//             <td> <button class="btnClear"></button>Editar</button>
+//             <td> <button class="btnClear"></button>Eliminar</button>
+//             </div>
+//             `;
+//       });
+//     })
+//     .catch((error) => {
+//       console.error('Error adding document: ', error);
+//     });
+//   };
+// */
